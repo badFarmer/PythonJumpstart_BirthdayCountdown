f5161db45a4e8a74e51becb978c1d530a09e33a7
badFarmer <guy@strategicmfg.com>
2019-01-02 20:05:32 -0800

    Initial Commit

--- program.py
+++ program.py
@@ -2,3 +2,10 @@ print('---------------------------------------------')
 print('           birthday countdown.               ')
 print('---------------------------------------------')
 print()
+
+birth_year = input('What year were you born? [YYYY] ')
+birth_month = input('Month? [MM] ')
+birth_day = input('Day? [DD] ')
+
+print('It looks like you were born on {}/{}/{}.'.format(birth_month, birth_day, birth_year))
+
