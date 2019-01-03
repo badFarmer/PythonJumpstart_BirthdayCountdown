1021b3f0dd5a3b05268a025cf8881a569e630f31
badFarmer <guy@strategicmfg.com>
2019-01-02 20:10:27 -0800

    Initial Commit

--- program.py
+++ program.py
@@ -1,11 +1,26 @@
-print('---------------------------------------------')
-print('           birthday countdown.               ')
-print('---------------------------------------------')
-print()
+def print_header():
+    print('---------------------------------------------')
+    print('           birthday countdown.               ')
+    print('---------------------------------------------')
+    print()
 
-birth_year = input('What year were you born? [YYYY] ')
-birth_month = input('Month? [MM] ')
-birth_day = input('Day? [DD] ')
+def get_birthday_from_user():
+    birth_year = input('What year were you born? [YYYY] ')
+    birth_month = input('Month? [MM] ')
+    birth_day = input('Day? [DD] ')
 
+
+def compute_days_between_dates():
+    pass
+
+def print_birthday_information():
+
+def main():
+    print_header()
+    get_birthday_from_user()
+    compute_days_between_dates()
+    print_birthday_information()
+
+get_birthday_from_user()
 print('It looks like you were born on {}/{}/{}.'.format(birth_month, birth_day, birth_year))
 
